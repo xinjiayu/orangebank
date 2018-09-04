7b0f6a106144bc99736b48c82c1bc9b3e862b2dc
wangrui <228226928@qq.com>
2018-09-04 17:26:44 +0800

    update pay callback

--- result.go
+++ result.go
@@ -1,13 +1,19 @@
 package orangebank
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 
+	"github.com/vgmdj/utils/chars"
 	"github.com/vgmdj/utils/logger"
 )
 
 type PayCallBack struct {
+	ErrCode     int    `json:"errcode"`
+	Msg         string `json:"msg"`
+	Data        string `json:"data"`
+	Timestamp   string `json:"timestamp"`
 	TradeType   string `json:"trade_type"`
 	OrdNo       string `json:"ord_no"`
 	OutNo       string `json:"out_no"`
@@ -18,14 +24,42 @@ type PayCallBack struct {
 	Sign        string `json:"sign"`
 	TradeResult string `json:"trade_result"`
 	OpenID      string `json:"open_id"`
-	Timestamp   string `json:"timestamp"`
 }
 
 const (
 	CallBackNotifySuccess = "notify_success"
 )
 
-func (pcb PayCallBack) CheckSign(openKey string) (err error) {
+func (pcb *PayCallBack) Parse(body []byte, openKey string) (err error) {
+	err = json.Unmarshal(body, pcb)
+	if err != nil {
+		logger.Error(err.Error())
+		return
+	}
+
+	aes := NewAES(openKey)
+	m, err := aes.Decrypt(pcb.Data)
+	if err != nil {
+		logger.Error(err.Error())
+		return err
+	}
+
+	pcb.TradeType = chars.ToString(m["trade_type"])
+	pcb.OrdNo = chars.ToString(m["ord_no"])
+	pcb.OutNo = chars.ToString(m["out_no"])
+	pcb.Status = chars.ToString(m["status"])
+	pcb.Amount = chars.ToString(m["amount"])
+	pcb.PayTime = chars.ToString(m["pay_time"])
+	pcb.RandStr = chars.ToString(m["rand_str"])
+	pcb.Sign = chars.ToString(m["sign"])
+	pcb.TradeResult = chars.ToString(m["trade_result"])
+	pcb.OpenID = chars.ToString(m["open_id"])
+
+	return pcb.CheckSign(openKey)
+
+}
+
+func (pcb *PayCallBack) CheckSign(openKey string) (err error) {
 	m := make(map[string]interface{})
 	m["trade_type"] = pcb.TradeType
 	m["ord_no"] = pcb.OrdNo
